88cc0fe57d59edcd389f68634e652b5c38027610
leonardo.amarilho <leonardo.amarilho@zenvia.com>
2018-06-14 17:56:59 -0300

    dojo reduce sample

--- pocs/README.md
+++ pocs/README.md
@@ -6,5 +6,7 @@
 ### REFERENCES
 
 > Optional
+
     https://www.voxxed.com/2015/02/defence-java-8s-optional-use/
+
     https://stackify.com/optional-java/
---
+++ pocs/src/main/java/com/dojo/Reduce.java
@@ -0,0 +1,20 @@
+package com.dojo;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class Reduce {
+
+
+    public Optional<BigDecimal> sampleReduce(Optional<BigDecimal> numberOne, Optional<BigDecimal> numberTwo){
+
+        return Stream.of(numberOne, numberTwo)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .reduce(BigDecimal::add);
+
+    }
+
+
+}
